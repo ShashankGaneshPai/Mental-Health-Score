d7af237125fa678ccfb9935d618191a97dcca109
ShashankGaneshPai <paishashank0123@gmail.com>
2026-09-05 22:31:06 +0530

    Update API_BASE URL for prediction service

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mental-health-score-qlrv.onrender.com";
+  const API_BASE = "https://mental-health-score-prediction-r43e.onrender.com/";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
